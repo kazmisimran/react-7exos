8261e801dd3241bc785888524f8b1272b8e733e4
Simran <kazmisimran@gmail.com>
2020-02-13 14:12:05 +0100

    exo1et2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "./../node_modules/bootstrap/dist/css/bootstrap.css";
 
-function App() {
+import Exo1 from "./exo1.jsx";
+import Exo2 from "./exo2.jsx";
+import Exo3 from './exo3.jsx';
+import Exo4 from "./exo4.jsx";
+import Exo5 from "./exo5.jsx";
+import Exo6 from "./exo6.jsx";
+import Exo7 from "./exo7.jsx";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="panel">
+          <ul>
+            <li>
+              <Link to="/">Exo1</Link>
+            </li>
+            <li>
+              <Link to="/exo2">Exo2</Link>
+            </li>
+            <li>
+              <Link to="/exo3">Exo3</Link>
+            </li>
+            <li>
+              <Link to="/exo4">Exo4</Link>
+            </li>
+            <li>
+              <Link to="/exo5">Exo5</Link>
+            </li>
+            <li>
+              <Link to="/exo6">Exo6</Link>
+            </li>
+            <li>
+              <Link to="/exo7">Exo7</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/exo2">
+            {/* <Exo2></Exo2> */}
+          </Route>
+          <Route path="/exo3">
+            {/* <Exo3></Exo3> */}
+          </Route>
+          <Route path="/exo4">
+            {/* <Exo4></Exo4> */}
+          </Route>
+          <Route path="/exo5">
+            {/* <Exo5></Exo5> */}
+          </Route>
+          <Route path="/exo6">
+            {/* <Exo6></Exo6> */}
+          </Route>
+          <Route path="/exo7">
+            {/* <Exo7></Exo7> */}
+          </Route>
+          <Route path="/">
+            <Exo1></Exo1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+
---
+++ src/exo1.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+
+export default function Exo1() {
+
+    let [titre, setTitre] = useState('Mon titre');
+    let [color, setColor] = useState(true);
+    let [bgColor, setBgColor] = useState(true);
+
+    return (
+        <div className="container my-5 bg-light h-50">
+            <h1 onMouseEnter={() => {
+                setTitre("Mon titre modifié");
+                setColor(false);
+                setBgColor(false)
+            }}
+                className={bgColor ? color ? "bg-light" : "bg-light text-danger" : color ? "bg-info" : "bg-info text-danger"}
+                onMouseLeave={() => {
+                    setTitre("Mon titre");
+                    setColor(true);
+                    setBgColor(true)
+                }}
+                id='titre'>{titre}</h1>
+            <p>Liste de props à retrouver dans la documentation: </p>
+            <ul>
+                <li>InnerText</li>
+                <li>nnerHTML</li>
+                <li>querySelector()</li>
+                <li>querySelectorAll()</li>
+                <li>style</li>
+                <li>classList</li>
+            </ul>
+            <p>
+                Changer le contenu du h2 par "le titre modifié"<br />
+                Changer la couleur  du h2 par la couleur que vous voulez <br />
+                Ajouter une class background bootstrap au h2
+            </p>
+        </div>
+    )
+}
